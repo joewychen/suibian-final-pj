f3c351e908cf157cdbb21a8b4ff11ff548fcceae
joewychen <powcca@berkeley.edu>
2021-04-27 17:35:39 -0700

    milestone

--- src/lightfield.py
+++ src/lightfield.py
@@ -23,12 +23,12 @@ class Lightfield:
             img = plt.imread(f)
             self.data.append(img)
         # print(self.data)
-        end = timeit.timeit()
-        print("Init takes " + str(end - start) + "seconds")
-        # self.image_size = self.data[0].size      #image -> each has (x, y) rgb vectors          
+        self.image_size = self.data[0].shape      #image -> each has (x, y) rgb vectors          
         self.data = np.array(self.data)
-        # print(self.data[:10])
-        # print(self.image_size)
+        end = timeit.timeit()
+        print(self.image_size)
+        print("Init takes " + str(end - start) + "seconds")
+
 
     def refocus(self, alpha = 0):
         # for img in range(SHAPE[0]*SHAPE[1]):
@@ -37,14 +37,26 @@ class Lightfield:
         # end = timeit.timeit()
         # print(end - start)
         # return ret
+
+        # self.output = []
+        # center = (SHAPE[0] // 2, SHAPE[1] // 2)
+        # for i in range(SHAPE[0]):
+        #     for j in range(SHAPE[1]):
+        #         dx = (i - center[0]) * -1
+        #         dy = (j - center[1]) * -1
+        #         shiftx = np.roll(self.data[i * SHAPE[1] + j], int(alpha * dx), axis = 0)
+        #         shifty = np.roll(shiftx, int(alpha * dy) // 3 * 3, axis = 1)
+        #         self.output.append(shifty)
+        # #self.output = np.array(self.output)
+        # return np.mean(self.output, axis = 0)
         self.output = []
         center = (SHAPE[0] // 2, SHAPE[1] // 2)
         for i in range(SHAPE[0]):
             for j in range(SHAPE[1]):
-                dx = abs(i - center[0])
-                dy = abs(j - center[1])
-                shiftx = np.roll(self.data[i * SHAPE[1] + j], int(alpha * dx), axis = 0)
-                shifty = np.roll(shiftx, int(alpha * dy), axis = 1)
+                dx = (i - center[0])
+                dy = (j - center[1]) * -1
+                shiftx = np.roll(self.data[(SHAPE[0] - 1 - i) * SHAPE[1] + SHAPE[1] - j - 1], int(alpha * dx), axis = 0)
+                shifty = np.roll(shiftx, int(alpha * dy) // 3 * 3, axis = 1)
                 self.output.append(shifty)
         self.output = np.array(self.output)
         return np.mean(self.output, axis = 0)
@@ -54,10 +66,10 @@ class Lightfield:
         pass
 
 def main():
-    lf = Lightfield("../temp/data/chess/")
+    lf = Lightfield("../scene/chess/")
     # print(lf.refocus())
     alpha = -1
-    for alpha in range(5):
+    for _ in range(5):
         print("writing the alpha =", alpha)
         ret = lf.refocus(alpha)
         plt.imsave("./results/" + "chess" + str(alpha) + ".png", ret)
